d8db43631acc2ad85c160653a716214cc60c207a
swc9803 <swc9803@gmail.com>
2021-08-19 17:30:16 +0900

    0819

--- src/pages/Box.vue
+++ src/pages/Box.vue
@@ -1,10 +1,129 @@
 <template>
-  <div class="front">
-    <img src="@/assets/박스위.png" class="boxup" @click="cutting">
-    <img src="@/assets/박스아래.png" class="boxdown" @click="cutting">
-    <img src="@/assets/커터칼.png" class="cut">
-    <img src="@/assets/라인.png" class="line">
-  </div>
+  <svg @click="cutting" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 1920 1080">
+    <defs/>
+    <g id="boxframe" clip-path="url(#clip0)">
+      <g id="boxbottom">
+        <path id="boxall" fill="url(#paint0_linear)" d="M0 540h1920v540H0z"/>
+        <path id="tape" fill="url(#paint1_linear)" d="M0 540h1920v90H0z"/>
+        <path id="boxline" stroke="#000" stroke-opacity=".2" stroke-width="1.5" d="M1920 540.75H0"/>
+      </g>
+      <g id="boxtop">
+        <path id="boxall_2" fill="url(#paint2_linear)" d="M0 0h1920v540H0z" transform="matrix(1 0 0 -1 0 540)"/>
+        <path id="tape_2" fill="url(#paint3_linear)" d="M0 0h1920v90H0z" transform="matrix(1 0 0 -1 0 540)"/>
+        <path id="boxline_2" stroke="#000" stroke-opacity=".2" stroke-width="1.5" d="M1920 539.25H0"/>
+      </g>
+      <path id="cutline" stroke="#000" stroke-opacity=".5" stroke-width="2" d="M1920 540H0"/>
+      <g id="knifegroup" filter="url(#filter0_d)">
+        <g id="body" filter="url(#filter1_d)">
+          <path fill="url(#paint4_linear)" d="M39.57 462.17L375 126.76a5 5 0 014.63-1.35l63.53 14.27a5 5 0 012.24 8.6L66.5 489.08l-14.38.62-12.54-27.53z"/>
+        </g>
+        <g id="knifebody">
+          <path id="knife" fill="#B8B8B8" d="M3.57 537.38L0 512.42l345.7-345.7 21.36 7.15-363.5 363.5z"/>
+          <g id="knife1" fill="#A6A6A6">
+            <path id="Rectangle 4" d="M28.51 512.43l-1.78 1.78-5.34-23.16 1.78-1.79 5.34 23.17z"/>
+            <path id="Rectangle 12" d="M199.57 341.37l-1.78 1.78-5.35-23.16 1.78-1.78 5.35 23.16z"/>
+            <path id="Rectangle 8" d="M114.04 426.9l-1.78 1.78-5.34-23.16 1.78-1.78 5.34 23.16z"/>
+            <path id="Rectangle 13" d="M285.1 255.84l-1.78 1.79-5.35-23.17 1.78-1.78 5.35 23.16z"/>
+            <path id="Rectangle 6" d="M71.28 469.67l-1.78 1.78-5.35-23.17 1.78-1.78 5.35 23.17z"/>
+            <path id="Rectangle 14" d="M242.34 298.6l-1.79 1.79-5.34-23.16 1.78-1.79 5.34 23.17z"/>
+            <path id="Rectangle 9" d="M156.8 384.14l-1.78 1.78-5.34-23.17 1.78-1.78 5.35 23.17z"/>
+            <path id="Rectangle 15" d="M327.86 213.08l-1.78 1.78-5.34-23.16 1.78-1.78 5.34 23.16z"/>
+            <path id="Rectangle 5" d="M49.9 491.05l-1.79 1.78-5.34-23.16 1.78-1.79 5.35 23.17z"/>
+            <path id="Rectangle 16" d="M220.95 320l-1.78 1.77-5.34-23.16 1.78-1.78 5.34 23.16z"/>
+            <path id="Rectangle 10" d="M135.42 405.52l-1.78 1.78-5.34-23.16 1.78-1.78 5.34 23.16z"/>
+            <path id="Rectangle 17" d="M306.48 234.46l-1.78 1.78-5.35-23.16 1.78-1.78 5.35 23.16z"/>
+            <path id="Rectangle 7" d="M92.66 448.28l-1.78 1.79-5.35-23.17 1.78-1.78 5.35 23.16z"/>
+            <path id="Rectangle 18" d="M263.72 277.23l-1.79 1.78-5.34-23.17 1.78-1.78 5.35 23.17z"/>
+            <path id="Rectangle 11" d="M178.19 362.75l-1.78 1.79-5.35-23.17 1.78-1.78 5.35 23.17z"/>
+            <path id="Rectangle 19" d="M349.25 191.7l-1.79 1.78-5.34-23.16 1.78-1.79 5.35 23.17z"/>
+          </g>
+        </g>
+        <g id="handle" filter="url(#filter2_d)">
+          <g id="Rectangle 2" filter="url(#filter3_d)">
+            <path fill="#323232" d="M217.41 331.9a5 5 0 01-7.07 0l-13.83-13.82a2 2 0 010-2.83l71.99-72a5 5 0 016.45-.52l17.48 12.52a1 1 0 01.12 1.52l-75.14 75.14z"/>
+          </g>
+          <g id="knifeline" stroke="#444" stroke-linecap="square">
+            <path id="Line 1" d="M285.72 262.04l-17.22-17.22m14.71 19.72l-8.6-8.6-8.61-8.61"/>
+            <path id="Line 3" d="M275.7 272.06l-17.21-17.22m14.7 19.72l-8.6-8.6-8.61-8.62"/>
+            <path id="Line 5" d="M265.68 282.07l-17.21-17.21m14.71 19.72l-8.6-8.61-8.62-8.6"/>
+            <path id="Line 7" d="M255.67 292.09l-17.22-17.22m14.71 19.72l-8.6-8.6-8.61-8.61"/>
+            <path id="Line 2" d="M280.7 267.05l-17.2-17.22m14.7 19.72l-8.6-8.6-8.61-8.61"/>
+            <path id="Line 4" d="M270.7 277.06l-17.22-17.21m14.7 19.72l-8.6-8.6-8.6-8.62"/>
+            <path id="Line 6" d="M260.68 287.08l-17.22-17.21m14.71 19.72l-8.6-8.61-8.61-8.61"/>
+            <path id="Line 8" d="M250.66 297.1l-17.22-17.22m14.72 19.72l-8.61-8.6-8.61-8.61"/>
+            <path id="Line 9" d="M245.65 302.1l-17.22-17.21m14.72 19.72l-8.61-8.6-8.61-8.62"/>
+            <path id="Line 10" d="M240.64 307.12l-17.21-17.22m14.7 19.72l-8.6-8.6-8.6-8.62"/>
+            <path id="Line 11" d="M235.63 312.12l-17.21-17.21m14.7 19.72l-8.6-8.61-8.6-8.6"/>
+            <path id="Line 12" d="M230.63 317.13l-17.22-17.21m14.71 19.72l-8.6-8.61-8.62-8.61"/>
+            <path id="Line 13" d="M225.62 322.14l-17.22-17.22m14.71 19.72l-8.6-8.6-8.61-8.61"/>
+          </g>
+        </g>
+      </g>
+    </g>
+    <defs>
+      <filter id="filter0_d" width="467.81" height="446.79" x="-14.69" y="98.59" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feColorMatrix in="SourceAlpha" result="hardAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+        <feOffset dy="4"/>
+        <feGaussianBlur stdDeviation="2"/>
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/>
+        <feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+      </filter>
+      <filter id="filter1_d" width="425.05" height="425.05" x="28.08" y="98.59" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feColorMatrix in="SourceAlpha" result="hardAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+        <feOffset dy="4"/>
+        <feGaussianBlur stdDeviation="2"/>
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/>
+        <feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+      </filter>
+      <filter id="filter2_d" width="104.92" height="99.58" x="191.93" y="241.79" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feColorMatrix in="SourceAlpha" result="hardAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+        <feOffset dy="4"/>
+        <feGaussianBlur stdDeviation="2"/>
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/>
+        <feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+      </filter>
+      <filter id="filter3_d" width="100.92" height="95.58" x="193.93" y="241.79" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feColorMatrix in="SourceAlpha" result="hardAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+        <feOffset dy="2"/>
+        <feGaussianBlur stdDeviation="1"/>
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/>
+        <feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+      </filter>
+      <linearGradient id="paint0_linear" x1="0" x2="829.33" y1="540" y2="1798.65" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#EBC9A4"/>
+        <stop offset="1" stop-color="#EBC9A4" stop-opacity=".8"/>
+      </linearGradient>
+      <linearGradient id="paint1_linear" x1="1568" x2="1247.6" y1="358.82" y2="1188" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#FFEBCB"/>
+        <stop offset="1" stop-color="#EED7B3"/>
+        <stop offset="1" stop-color="#FFE1B3"/>
+      </linearGradient>
+      <linearGradient id="paint2_linear" x1="0" x2="829.33" y1="0" y2="1258.65" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#EBC9A4"/>
+        <stop offset="1" stop-color="#EBC9A4" stop-opacity=".8"/>
+      </linearGradient>
+      <linearGradient id="paint3_linear" x1="1568" x2="1247.6" y1="-181.18" y2="648" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#FFEBCB"/>
+        <stop offset="1" stop-color="#EED7B3"/>
+        <stop offset="1" stop-color="#FFE1B3"/>
+      </linearGradient>
+      <linearGradient id="paint4_linear" x1="42.99" x2="447.35" y1="483.72" y2="84.49" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#599CFF"/>
+        <stop offset="1" stop-color="#3B7FE6"/>
+      </linearGradient>
+      <clipPath id="clip0">
+        <path fill="#fff" d="M0 0h1920v1080H0z"/>
+      </clipPath>
+    </defs>
+  </svg>
 </template>
 
 <script>
@@ -37,7 +156,7 @@ export default {
       })
       setTimeout(function () {
         router.push({
-          name: 'Sea'
+          name: 'Weather'
         })
       }, 6500)
     }
@@ -49,37 +168,18 @@ export default {
 </script>
 
 <style scoped>
-.front {
-  overflow-x: hidden;
-  overflow-y: hidden;
+body,
+html{
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+.svg {
+  display: block;
+  width: 100%;
+  height: 100%;
+  position: fixed;
   top: 0;
   left: 0;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-}
-.boxup {
-  cursor: pointer;
-  position: absolute;
-  width: 100%;
-  height: 50%;
-}
-.boxdown {
-  top: 50%;
-  cursor: pointer;
-  position: absolute;
-  width: 100%;
-  height: 50%;
-}
-.cut {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  visibility: hidden;
-}
-.line {
-  position: absolute;
-  width: 0.1px;
-  height: 100%;
 }
 </style>
